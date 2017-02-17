f241b00cd52b6dc14ac04ee6046a5cc7b170bb5b
Anveshak <anveshak@Anveshaks-iMac.local>
2017-02-16 19:55:44 -0800

    Initial Commit

---
+++ Veg_Expenses2/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Veg_Expenses2
+//
+//  Created by Anveshak on 27/11/1938 Saka.
+//  Copyright © 1938 Saka Anveshak. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Veg_Expenses2/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Veg_Expenses2
+//
+//  Created by Anveshak on 27/11/1938 Saka.
+//  Copyright © 1938 Saka Anveshak. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
